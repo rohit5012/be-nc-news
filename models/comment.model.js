00a69d28c06d3f225794a331f65c7a17b73a01c6
rshaarma <rohittsharrma@outlook.com>
2024-11-27 16:31:28 +0000

    9-delete-api-comments-comment_id

--- models/comment.model.js
+++ models/comment.model.js
@@ -32,3 +32,18 @@ exports.postComments = (username, body, article_id) => {
       return rows[0];
     });
 };
+
+exports.commentDeleted = (comment_id) => {
+  return db
+    .query(`DELETE FROM comments WHERE comment_id = $1  RETURNING *;`, [
+      comment_id,
+    ])
+    .then(({ rows }) => {
+      if (!rows[0]) {
+        return Promise.reject({
+          status: 404,
+          msg: "Not Found",
+        });
+      }
+    });
+};
