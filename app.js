fa0ce2161ff670e47d388951f88743a7146b2742
rshaarma <rohittsharrma@outlook.com>
2024-12-16 12:10:51 +0000

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const {
   handleCustomErrors,
@@ -6,6 +7,7 @@ const {
   handlePostgreErrors,
 } = require("./errors/index");
 
+app.use(cors());
 app.use(express.json());
 
 const { getApi } = require("./controllers/api.controller");
